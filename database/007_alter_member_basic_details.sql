9fe1f99809c85bf869f6241154951860fd72748e
Anant Singh <anant.singh@copters.in>
2026-07-26 11:49:25 +0530

    SQL Changes

--- database/007_alter_member_basic_details.sql
+++
@@ -1,5 +0,0 @@
-ALTER TABLE member_basic_details
-ADD COLUMN IF NOT EXISTS about_me TEXT NULL;
-
-COMMENT ON COLUMN member_basic_details.about_me IS
-    'Member-written plain-text profile introduction, maximum 500 words.';
